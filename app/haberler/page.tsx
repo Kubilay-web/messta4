ddbf55c10a6092834f9add806f8abe7bedad7b1f
Kubilay-web <kubilayozdemir95@gmail.com>
2026-06-29 14:28:24 +0300

    Updated

--- app/haberler/page.tsx
+++ app/haberler/page.tsx
@@ -2,14 +2,13 @@
 
 import Header from "@/app/projects/components/newsportal/Header";
 import Footer from "@/app/projects/components/newsportal/Footer";
-import News from "../(components)/(content-layout)/home/news/page";
 
 export default async function HaberlerPage() {
   return (
     <>
       <div>
         <Header />
-        <News />
+        {/* <News /> */}
         <Footer />
       </div>
     </>
